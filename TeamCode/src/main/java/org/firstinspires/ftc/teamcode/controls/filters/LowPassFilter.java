85ef3eb04389107b716d3c93e026e24dc697921c
AiSHA <142852320+witchhazelz@users.noreply.github.com>
2024-11-24 18:29:14 -0500

    Create LowPassFilter.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controls/filters/LowPassFilter.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.controls.filters;
+
+public class LowPassFilter {
+    private double a;
+    private double previousEstimate;
+
+    public LowPassFilter(double a) {
+        this.a = a;
+        this.previousEstimate = 0;
+    }
+
+    public double filter(double measurement) {
+        double currentEstimate = (a * previousEstimate) + (1 - a) * measurement;
+        previousEstimate = currentEstimate;
+        return currentEstimate;
+    }
+}
