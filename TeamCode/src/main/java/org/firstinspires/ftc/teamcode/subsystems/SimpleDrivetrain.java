adb424c7e3c1c75ee214f3ebe3df3ddbf0fc6ecb
NoT-MaRYaM <othman_maryam@icloud.com>
2025-01-02 21:59:53 -0500

    nthn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SimpleDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SimpleDrivetrain.java
@@ -24,7 +24,35 @@ public class SimpleDrivetrain {
 
     }
 
+    private void setPowers(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower) {
+         double maxSpeed = 1.0;
+         maxSpeed = Math.max(maxSpeed, Math.abs(frontLeftPower));
+         maxSpeed = Math.max(maxSpeed, Math.abs(frontRightPower));
+         maxSpeed = Math.max(maxSpeed, Math.abs(backLeftPower));
+         maxSpeed = Math.max(maxSpeed, Math.abs(backRightPower));
+
+         frontLeftPower /= maxSpeed;
+         frontRightPower /= maxSpeed;
+         backLeftPower /= maxSpeed;
+         backRightPower /= maxSpeed;
+
+         frontLeft.setPower(frontLeftPower);
+         frontRight.setPower(frontRightPower);
+         backLeft.setPower(backLeftPower);
+         backRight.setPower(backRightPower);
+        }
+
+         // Thanks to FTC16072 for sharing this code!!
+         public void drive(double forward, double right, double rotate) {
+         double frontLeftPower = forward + right + rotate;
+         double frontRightPower = forward - right - rotate;
+         double backLeftPower = forward - right + rotate;
+         double backRightPower = forward + right - rotate;
+
+        setPowers(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
     }
+
+
     public void printTelemetry() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
