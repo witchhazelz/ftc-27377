214b0ae3bbb1b98f89c7f8b3e411e8819c83764e
AiSHA <142852320+witchhazelz@users.noreply.github.com>
2025-01-21 00:37:24 -0500

    the sigma....LOCK IN!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Linear.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
